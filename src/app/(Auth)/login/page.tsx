d046965f4e865ec66fd1cded6e39b1af7680cd1c
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2023-09-08 02:26:46 -0400

    fixed attribute naming and label naming

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
           htmlFor='floating_email'
           className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
         >
-          Username
+          Email
         </label>
       </div>
       <div className='relative z-0 w-full mb-6 group'>
--- src/app/(Auth)/register/page.tsx
+++ src/app/(Auth)/register/page.tsx
@@ -24,15 +24,15 @@ export default function Register() {
       </div>
       <div className='relative z-0 w-full mb-6 group'>
         <input
-          type='email'
-          name='username'
-          id='username'
+          type='text'
+          name='floating_username'
+          id='floating_username'
           className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
           placeholder=' '
           required
         />
         <label
-          htmlFor='username'
+          htmlFor='floating_username'
           className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
         >
           Username
@@ -111,5 +111,5 @@ export default function Register() {
         Submit
       </button>
     </form>
-  );
+  )
 }

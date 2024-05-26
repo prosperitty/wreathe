6b0623cef2b9eda9d66f9c258aa4619b5b947aef
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-05-26 03:13:55 -0400

    updated name attribute naming convention

--- src/app/(Auth)/register/components/form.tsx
+++ src/app/(Auth)/register/components/form.tsx
@@ -74,14 +74,14 @@ export default function Form(props: Props) {
       <div className="relative z-0 w-full mb-6 group">
         <input
           type="password"
-          name="repeat-password"
+          name="repeat_password"
           id="floating_repeat_password"
           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" "
           required
         />
         <label
-          htmlFor="repeat-password"
+          htmlFor="repeat_password"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Confirm password
@@ -91,14 +91,14 @@ export default function Form(props: Props) {
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
-            name="first-name"
+            name="first_name"
             id="floating_first_name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
           <label
-            htmlFor="first-name"
+            htmlFor="first_name"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             First name
@@ -107,14 +107,14 @@ export default function Form(props: Props) {
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
-            name="last-name"
+            name="last_name"
             id="floating_last_name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
           <label
-            htmlFor="last-name"
+            htmlFor="last_name"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
             Last name

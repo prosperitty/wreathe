01131b02cd0f1f95a57fd767698265f5ea0679e2
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-05-27 23:23:34 -0400

    added header to conditional

--- src/app/(Content)/feed/page.tsx
+++ src/app/(Content)/feed/page.tsx
@@ -59,6 +59,32 @@ export default async function Feed({
   } else if (result.threads.length === 0 && accessToken) {
     return (
       <>
+        <Header>
+          <div className="relative w-full rounded-md">
+            <Suspense
+              key={query}
+              fallback={
+                <div className="absolute w-full flex p-2 animate-pulse rounded-md bg-gray-900 border border-gray-600">
+                  <svg
+                    className="w-8 h-8 text-gray-200 dark:text-gray-700 me-4"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                  </svg>
+                  <div className="space-y-2 self-center">
+                    <div className="w-20 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                    <div className="w-20 h-2 bg-gray-200 rounded-full dark:bg-gray-700 "></div>
+                  </div>
+                </div>
+              }
+            >
+              <UserList query={query} />
+            </Suspense>
+          </div>
+        </Header>
         <main className="p-2 sm:p-4 h-full">
           <h1 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
             Welcome to Wreathe.

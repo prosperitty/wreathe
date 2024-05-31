0f91cc5f3608d7c0ad9aca28120b386fd658b371
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-05-31 02:53:58 -0400

    types: fixed type error bug

--- src/app/(Content)/users/[Username]/threads/[thread_uid]/components/Comments.tsx
+++ src/app/(Content)/users/[Username]/threads/[thread_uid]/components/Comments.tsx
@@ -10,44 +10,48 @@ interface Props {
 }
 
 export default function CommentsSection(props: Props) {
-  let { userData } = useAuthContext()
-  let [commentList, setCommentList] = useState<JSX.Element[]>([])
+  const { userData } = useAuthContext()
+  const [commentList, setCommentList] = useState<JSX.Element[]>([])
 
   useEffect(() => {
-    if (userData !== null) {
-      const commentList = props.comments.map((post) => {
-        const isLiked = post.comment_likes.some(
+    if (userData) {
+      setCommentList(personalizedComments)
+    } else {
+      setCommentList(comments)
+    }
+  }, [props.comments, props.threadAuthor, userData])
+
+  const personalizedComments = props.comments.map((post) => {
+    const isLiked = userData
+      ? post.comment_likes.some(
           (like) =>
             like.user_uid === userData.user_uid &&
             like.comment_uid === post.comment_uid,
         )
-        console.log(isLiked)
-        return (
-          <li key={post.comment_uid}>
-            <Comment
-              commentData={post}
-              threadAuthor={props.threadAuthor}
-              likeButton={<LikeButton isLiked={isLiked} commentData={post} />}
-            />
-          </li>
-        )
-      })
-      setCommentList(commentList)
-    } else {
-      const commentList = props.comments.map((post) => {
-        return (
-          <li key={post.comment_uid}>
-            <Comment
-              commentData={post}
-              threadAuthor={props.threadAuthor}
-              likeButton={<LikeButton isLiked={false} commentData={post} />}
-            />
-          </li>
-        )
-      })
-      setCommentList(commentList)
-    }
-  }, [props.comments, props.threadAuthor, userData])
+      : false
+    console.log(isLiked)
+    return (
+      <li key={post.comment_uid}>
+        <Comment
+          commentData={post}
+          threadAuthor={props.threadAuthor}
+          likeButton={<LikeButton isLiked={isLiked} commentData={post} />}
+        />
+      </li>
+    )
+  })
+
+  const comments = props.comments.map((post) => {
+    return (
+      <li key={post.comment_uid}>
+        <Comment
+          commentData={post}
+          threadAuthor={props.threadAuthor}
+          likeButton={<LikeButton isLiked={false} commentData={post} />}
+        />
+      </li>
+    )
+  })
 
   return <ul>{commentList}</ul>
 }

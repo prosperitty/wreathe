c5b5393b5fc01658b8f85c4247bb8e25f1af3521
Alex Marmolejo <42818181+alex-lvl@users.noreply.github.com>
2024-06-04 03:35:54 -0400

    removed config logic

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `https://${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
